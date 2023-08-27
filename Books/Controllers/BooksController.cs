18a5419869769a5a25200d6e16a2c96ecc4b983a
Kareem Adel <adel30320@gmail.com>
2023-08-27 11:02:32 +0300

    Add project files.

---
+++ Books/Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Books.Models;
+using Books.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Books.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly ApplicationDBcontext _dbcontext;
+
+        public BooksController()
+        {
+            _dbcontext = new ApplicationDBcontext();
+        }
+
+        // GET: Books
+        public ActionResult Index()
+        {
+            var books = _dbcontext.Books.Include(m=>m.Category).ToList();
+            return View(books);
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new BookFormViewModel
+            {
+                Categories = _dbcontext.Categories.Where(m => m.IsActive).ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(BookFormViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Categories = _dbcontext.Categories.Where(m => m.IsActive).ToList();
+                return View("Create" ,model);
+            }
+            var book = new Book
+            {
+                Title =model.Title,
+                Author =model.Author,
+                CategoryId =model.CategoryId,
+                Discraption =model.Discraption
+            };
+            _dbcontext.Books.Add(book);
+            _dbcontext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Books/Models/ApplicationDBcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Books.Models
+{
+    public class ApplicationDBcontext :DbContext
+    {
+        public ApplicationDBcontext() :base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
