ef2f239fa71b6fdf782f36cfdd984300c70db83b
Kareem Adel <adel30320@gmail.com>
2023-08-28 14:23:50 +0300

    Api Book and create action DeleteBook  and added bootbox

--- Books/App_Start/BundleConfig.cs
+++ Books/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace Books
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.min.js",
                        "~/Scripts/toastr.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
---
+++ Books/Controllers/Api/BooksController.cs
@@ -0,0 +1,31 @@
+﻿using Books.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Books.Controllers.Api
+{
+    public class BooksController : ApiController
+    {
+        private readonly ApplicationDBcontext _dbcontext;
+
+        public BooksController()
+        {
+            _dbcontext = new ApplicationDBcontext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteBook(int id)
+        {
+            var book = _dbcontext.Books.Find(id);
+            if (book == null)
+                return NotFound();
+            _dbcontext.Books.Remove(book);
+            _dbcontext.SaveChanges();   
+            return Ok();
+        }
+    }
+}
